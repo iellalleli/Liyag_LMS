ab8f093343319fb4eaf0ffbd8410fcc22e0a055e
Pengskii09 <yojie9@gmail.com>
2025-07-02 21:03:46 +0800

    Update form.blade.php

--- resources/views/quotations/partials/form.blade.php
+++ resources/views/quotations/partials/form.blade.php
@@ -2,6 +2,8 @@
     $isEdit = isset($quotation);
     $user = Auth::user();
     $isClient = $user && $user->hasRole('client');
+    $user = Auth::user();
+    $isClient = $user && $user->hasRole('client');
 @endphp
 
 <div class="mb-3">
@@ -38,6 +40,7 @@
         <option value="">-- Select --</option>
         @foreach (['Email', 'Phone', 'Messenger'] as $method)
             <option value="{{ $method }}" {{ old('communication_method', $isEdit ? $quotation->communication_method : '') == $method ? 'selected' : '' }}>
+            <option value="{{ $method }}" {{ old('communication_method', $isEdit ? $quotation->communication_method : '') == $method ? 'selected' : '' }}>
                 {{ $method }}
             </option>
         @endforeach
@@ -57,11 +60,15 @@
     <select name="budget_range" class="form-control" required>
         <option value="">-- Select Range --</option>
         <option value="under_50k" {{ old('budget_range', $isEdit ? $quotation->budget_range : '') == 'under_50k' ? 'selected' : '' }}>
+        <option value="under_50k" {{ old('budget_range', $isEdit ? $quotation->budget_range : '') == 'under_50k' ? 'selected' : '' }}>
             Under ₱50,000</option>
+        <option value="50k_100k" {{ old('budget_range', $isEdit ? $quotation->budget_range : '') == '50k_100k' ? 'selected' : '' }}>
         <option value="50k_100k" {{ old('budget_range', $isEdit ? $quotation->budget_range : '') == '50k_100k' ? 'selected' : '' }}>
             ₱50,000 - ₱100,000</option>
         <option value="100k_200k" {{ old('budget_range', $isEdit ? $quotation->budget_range : '') == '100k_200k' ? 'selected' : '' }}>
+        <option value="100k_200k" {{ old('budget_range', $isEdit ? $quotation->budget_range : '') == '100k_200k' ? 'selected' : '' }}>
             ₱100,000 - ₱200,000</option>
+        <option value="200k_plus" {{ old('budget_range', $isEdit ? $quotation->budget_range : '') == '200k_plus' ? 'selected' : '' }}>
         <option value="200k_plus" {{ old('budget_range', $isEdit ? $quotation->budget_range : '') == '200k_plus' ? 'selected' : '' }}>
             Above ₱200,000</option>
     </select>
@@ -80,19 +87,25 @@
         <input type="checkbox" name="package_deal" value="1" class="form-check-input" {{ old('package_deal', $isEdit ? $quotation->package_deal : false) ? 'checked' : '' }}>
         <label class="form-check-label">Package Deal (includes photography and videography, customized giveaways, and
             more!)</label>
+        <input type="checkbox" name="package_deal" value="1" class="form-check-input" {{ old('package_deal', $isEdit ? $quotation->package_deal : false) ? 'checked' : '' }}>
+        <label class="form-check-label">Package Deal (includes photography and videography, customized giveaways, and
+            more!)</label>
     </div>
 
     <div class="form-check">
+        <input type="checkbox" name="catering" value="1" class="form-check-input" {{ old('catering', $isEdit ? $quotation->catering : false) ? 'checked' : '' }}>
         <input type="checkbox" name="catering" value="1" class="form-check-input" {{ old('catering', $isEdit ? $quotation->catering : false) ? 'checked' : '' }}>
         <label class="form-check-label">Catering</label>
     </div>
 
     <div class="form-check">
         <input type="checkbox" name="hair_makeup" value="1" class="form-check-input" {{ old('hair_makeup', $isEdit ? $quotation->hair_makeup : false) ? 'checked' : '' }}>
+        <input type="checkbox" name="hair_makeup" value="1" class="form-check-input" {{ old('hair_makeup', $isEdit ? $quotation->hair_makeup : false) ? 'checked' : '' }}>
         <label class="form-check-label">Hair & Makeup</label>
     </div>
 
     <div class="form-check">
+        <input type="checkbox" name="live_band" value="1" class="form-check-input" {{ old('live_band', $isEdit ? $quotation->live_band : false) ? 'checked' : '' }}>
         <input type="checkbox" name="live_band" value="1" class="form-check-input" {{ old('live_band', $isEdit ? $quotation->live_band : false) ? 'checked' : '' }}>
         <label class="form-check-label">Live Band</label>
     </div>
